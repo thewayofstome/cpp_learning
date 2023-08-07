e61ca6d6ae37e3b9307a928baebc860b1cee0847
thewayofstome <3268234598@qq.com>
2023-08-07 20:42:01 +0800

    modified:   stl_learning/introduction_templates.cpp

--- stl_learning/introduction_templates.cpp
+++ stl_learning/introduction_templates.cpp
@@ -21,6 +21,27 @@ T square(T x)
 	return x*x;
 }
 
+//使用模板构造一个简单的动态数组
+//Using templates to construct a simple dynamic array
+template <typename T>
+class BoArray{
+	T arr[1000];
+	int size;
+public:
+	BoArray():size(0){} //构造函数Constructor
+	void push(T x){ //填入元素
+		arr[size]=x;
+		size++;
+	}
+	
+	void print(){ //打印元素
+		for(int i=0;i<size;i++){
+			//print
+			std::cout<<arr[i]<<"\n";
+		}
+	}
+};
+
 int main()
 {
 //	first we create a int return value function
@@ -29,6 +50,15 @@ int main()
 //	a useful way is overwrite this function
 	std::cout<<square(5.5)<<"\n";
 //	than we want use same function to  calculate any type value
+	
+//	you can try anther type
+	BoArray<float> Array;
+	for(int i=0;i<10;i++){
+		//push element
+		//填入元素
+		Array.push(i);
+	}
+	Array.print();
 	
 	return 0;
 }
