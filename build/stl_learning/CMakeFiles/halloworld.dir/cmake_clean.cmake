bfcfaf7a08f797dbf584d3e8e729c84c5875944c
thewayofstome <3268234598@qq.com>
2023-12-14 21:41:39 +0800

    new file:   stl_learning/CMakeLists.txt

---
+++ build/stl_learning/CMakeFiles/halloworld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/halloworld.dir/halloworld.cpp.obj"
+  "CMakeFiles/halloworld.dir/halloworld.cpp.obj.d"
+  "halloworld.exe"
+  "halloworld.exe.manifest"
+  "halloworld.pdb"
+  "libhalloworld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/halloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/stl_learning/CMakeFiles/halloworld.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for halloworld.
+# This may be replaced when dependencies are built.
